ec6a26d6ddbc938ce7968d233bd416986bad975c
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2024-01-16 22:02:01 -0300

    Entrega 1

--- src/ProductManager.ts
+++ src/ProductManager.ts
@@ -1,29 +1,59 @@
 import { Product } from "./core/interfaces/product.interface";
 
 export class ProductManager {
-  products:Product[] = [];
-  product!:Product;
-  idSig:number = 1;
+  private products: Product[] = [];
+  private idSig: number = 1;
 
-  construct(product:Product) {
-    const { title, description, price, thumbnail, code, stock } = product;
-    this.product.title = title;
-    this.product.description = description;
-    this.product.price = price;
-    this.product.thumbnail = thumbnail;
-    this.product.code = code;
-    this.product.stock = stock;
-  }
+  construct() { }
 
-  addProduct(product:Product) {
-      if (!this.products.includes()){
-          product.id = this.idSig;
-          product.id = this.products.push(product);
-          this.idSig++;
+  public addProduct(product: Product): void {
+    if (this.validateRequiredFields(product)) {
+      if (!this.validateCode(product.code)) {
+        product.id = this.idSig++;
+        product.id = this.products.push(product);
+      } else {
+        console.error(`Error: Ya existe un producto con el código ${product.code}`);
+        throw new Error(`Error: Ya existe un producto con el código ${product.code}`);
       }
+    } else {
+      console.error('Error: El producto no es válido. Todos los campos son requeridos.');
+      throw new Error('Error: El producto no es válido. Todos los campos son requeridos.');
+    }
   }
 
-  private validarCodigoExistente() {
+  public getProducts(): Product[] {
+    return this.products;
+  }
 
+  public getProductById(id: number): Product | undefined {
+    let product = this.products.find(elem => elem.id === id);
+    if (product) {
+      return product;
+    } else {
+      console.error(`No existe un producto con id ${id}`);
+      return undefined;
+    }
+  }
+
+  private validateCode(code: number): boolean {
+    return this.products.some(elem => elem.code === code);
+  }
+
+  private isValidString(value: string | undefined): boolean {
+    return typeof value === 'string' && value.trim() !== '';
+  }
+
+  private isValidNumber(value: number | undefined): boolean {
+    return typeof value === 'number' && !isNaN(value);
+  }
+
+  private validateRequiredFields(product: Product): boolean {
+    const { title, description, price, thumbnail, code, stock } = product;
+    return (this.isValidString(title) &&
+      this.isValidString(description) &&
+      this.isValidNumber(price) &&
+      this.isValidString(thumbnail) &&
+      this.isValidNumber(code) &&
+      this.isValidNumber(stock));
   }
 }
--- src/core/interfaces/product.interface.ts
+++ src/core/interfaces/product.interface.ts
@@ -1,9 +1,9 @@
-export class Product{
-  title:string = "";
-  description:string = "";
-  price:number = 0;
-  thumbnail:string = "";
-  code!:number;
-  stock:number = 0;
-  id!:number;
+export interface Product{
+  title:string;
+  description:string;
+  price:number;
+  thumbnail:string;
+  code:number;
+  stock:number;
+  id:number;
 }
