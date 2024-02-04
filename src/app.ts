a968f8ff50843a15469bd628e8e78270fe510d70
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2024-02-04 10:35:15 -0300

    Instalo express. Creo servidor básico en app.ts.

--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,12 @@
+import express from 'express';
 import { ProductManager } from '@core/ProductManager';
 
+const app = express();
+const PORT = 3000;
 const productManager = new ProductManager('./src/data');
 
-const product = {
-  title: 'producto1',
-  description: 'descripcion1',
-  price: 40,
-  thumbnail: 'asdasd.jpg',
-  code: 1,
-  stock: 80,
-  id: 0,
-};
+app.get('/products', (req, res) => {
+  res.send('Hello world!');
+});
 
-productManager.addProduct(product);
-
-const product2 = {
-  title: 'producto2',
-  description: 'descripcion2',
-  price: 50,
-  thumbnail: 'asdasd2.jpg',
-  code: 2,
-  stock: 150,
-  id: 0,
-};
-
-productManager.addProduct(product2);
-
-console.log(
-  `getProducts: ${JSON.stringify(productManager.getProducts(), null, 2)}`,
-);
-
-console.log(
-  `getProductById(2): ${JSON.stringify(productManager.getProductById(2), null, 2)}`,
-);
-
-const updateProduct = {
-  description: 'Descripcion actualizada',
-  stock: 100,
-};
-
-productManager.updateProduct(2, updateProduct);
-
-console.log(
-  `getProducts post update: ${JSON.stringify(productManager.getProducts(), null, 2)}`,
-);
-
-productManager.deleteProduct(2);
-
-console.log(
-  `getProducts post delete: ${JSON.stringify(productManager.getProducts(), null, 2)}`,
-);
+app.listen(PORT, () => console.log(`The server is listening on port ${PORT}.`));
