ef7c2eb59680e19c2e354add342f6956de89395c
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2024-03-10 21:16:03 -0300

    Instalo Joi. Termino de desarrollar el DAO para products: Creo un controller que se encarga de las solicitudes HTTP, y creo services de product para filesystem y para db (FALTA TESTEAR).

--- desafio_06_02/src/index.ts
+++ desafio_06_02/src/index.ts
@@ -4,12 +4,22 @@ import productRoutes from './routes/product.routes';
 import cartRoutes from './routes/cart.routes';
 import userRoutes from './routes/user.routes';
 import viewsRoutes from './routes/views.routes';
+import { createDirectory, createFile } from './services/fileSystem.services';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const ROUTE = __dirname + '/data';
+const FILENAME = path.join(ROUTE, '/products.json');
+const PORT = process.env.PORT || 8080;
+
+createDirectory(ROUTE);
+createFile(FILENAME, '[]');
 
 app.use('/api/products', productRoutes);
 app.use('/api/carts', cartRoutes);
 app.use('/api/users', userRoutes);
 app.use('/', viewsRoutes);
-
-const PORT = 8080;
 
 app.listen(PORT, () => console.log('Server listen on port ', PORT));
