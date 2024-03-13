684b43aad73ac8c1322e06431ed624a61c9d29e4
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2024-03-13 13:08:06 -0300

    Comienzo con chat.

--- desafio_06_02/src/routes/views.routes.ts
+++ desafio_06_02/src/routes/views.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 import {
   renderProducts,
   renderRealTimeProducts,
-} from '../controllers/views.controller';
+} from '@controllers/views.controller';
+import { getMessages } from '@controllers/message.controller';
 
 const router = Router();
 
 router.get('/', renderProducts);
 
 router.get('/realtimeproducts', renderRealTimeProducts);
+
+router.get('/messages', getMessages);
 
 export default router;
