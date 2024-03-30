2f47be3166eee7ede12e312dd31392288ac33613
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2024-03-30 17:58:20 -0300

    Comienzo a desarrollar vista products y su logica.

--- desafio_08/src/controllers/views.controller.ts
+++ desafio_08/src/controllers/views.controller.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from 'express';
-import { getProducts } from '@controllers/products.controller';
+import { getProducts } from '@services/dao/db/product.service';
 
 //TODO: Desarrollar controladores
-export const renderProducts = async (req: Request, res: Response) => {};
+export const renderProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await getProducts(10, 1, undefined, undefined, undefined);
+    console.log('Primer producto:', result.data.docs[0]);
+    res.status(result.status).render('home', { products: result.data.docs });
+  } catch (error: any) {
+    res.status(error.status).json({ status: 'FAILED', message: error.message });
+  }
+};
 export const renderRealTimeProducts = async (req: Request, res: Response) => {};
